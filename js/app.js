bbe0174aed7d71abaa7dfc7ec2516dde5987edf8
lazar-the-coder <120413005+lazar-the-coder@users.noreply.github.com>
2023-04-15 17:33:48 -0500

    Finished
    For exceeds

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
-
-for ()
+const  titles = ["Hay Bales", "Lake", "Canyon", "Iceberg", "Desert", "Fall", "Plantation", "Dunes", "Countryside Lane", "Sunset", "Cave", "Bluebells"]
+const  captions = [
+    "I love hay bales. Took this snap on a drive through the countryside past some straw fields.",
+    "The lake was so calm today. We had a great view of the snow on the mountains from here.",
+    "I hiked to the top of the mountain and got this picture of the canyon and trees below.",
+    "It was amazing to see an iceberg up close, it was so cold but didn’t snow today.",
+    "The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons.",
+    "Fall is coming, I love when the leaves on the trees start to change color.",
+    "I drove past this plantation yesterday, everything is so green!",
+    "My summer vacation to the Oregon Coast. I love the sandy dunes!",
+    "We enjoyed a quiet stroll down this countryside lane.",
+    "Sunset at the coast! The sky turned a lovely shade of orange.",
+    "I did a tour of a cave today and the view of the landscape below was breathtaking.",
+    "I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in."
+]
+let html = '';
+for (let i = 1; i <= 12; i++) {
+    if (i < 10) {
+        html += `<a href="photos/0${i}.jpg" data-caption="${captions[i-1]}">
+            <img src="photos/thumbnails/0${i}.jpg" alt="${titles[i-1]}">
+        </a>`;
+    } else {
+        html += `<a href="photos/${i}.jpg" data-caption="${captions[i-1]}">
+            <img src="photos/thumbnails/${i}.jpg" alt="${titles[i-1]}">
+        </a>`;
+    }
+}
+document.querySelector('.gallery').innerHTML = html;
 
 baguetteBox.run('.gallery');
-const search = new Filter('search', 'data-caption');
---
+++ js/searcherFilter.js
@@ -0,0 +1,15 @@
+function searchItem() {
+    let filter = document.getElementById('search').value.toLowerCase();
+    let gallery = document.getElementsByClassName('gallery')[0];
+    let photoList = gallery.getElementsByTagName('a');
+
+    for (i = 0; i < photoList.length; i++) {
+        let photo = photoList[i];
+        let caption = photo.getAttribute("data-caption");
+        if (caption.includes(filter)) {
+            photo.style.display = "";
+        } else {
+            photo.style.display = "none";
+        };
+    };
+}
